3be53944c1741eba7007e4e0260ba7633ed082cd
Rishabh Anand <rishabhanand42@gmail.com>
2026-05-16 17:40:01 +0530

    Adding pg, azure blob: checking normal builds are fine

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,9 +1,26 @@
 import type { Core } from '@strapi/strapi';
 
+const blobHost = process.env.STORAGE_ACCOUNT
+  ? `${process.env.STORAGE_ACCOUNT}.blob.core.windows.net`
+  : null;
+
 const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', ...(blobHost ? [blobHost] : [])],
+          'media-src': ["'self'", 'data:', 'blob:', ...(blobHost ? [blobHost] : [])],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,27 @@
 import type { Core } from '@strapi/strapi';
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => {
+  const storageAccount = env('STORAGE_ACCOUNT');
+
+  if (!storageAccount) {
+    return {};
+  }
+
+  return {
+    upload: {
+      config: {
+        provider: 'strapi-provider-upload-azure-storage',
+        providerOptions: {
+          authType: 'default',
+          account: storageAccount,
+          accountKey: env('STORAGE_ACCOUNT_KEY'),
+          serviceBaseURL: env('STORAGE_URL'),
+          containerName: env('STORAGE_CONTAINER_NAME', 'media'),
+          defaultPath: 'uploads',
+        },
+      },
+    },
+  };
+};
 
 export default config;
