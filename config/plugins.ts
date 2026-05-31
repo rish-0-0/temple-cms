b7aeebbd6c03b23e593d96c7e28fb6e1851e8d01
Rishabh Anand <rishabhanand42@gmail.com>
2026-05-31 23:13:58 +0530

    Entra Login

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,11 +3,28 @@ import type { Core } from '@strapi/strapi';
 const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => {
   const storageAccount = env('STORAGE_ACCOUNT');
 
+  const plugins: Core.Config.Plugin = {
+    'strapi-plugin-sso': {
+      enabled: true,
+      config: {
+        MICROSOFT: {
+          ENABLE: true,
+          CLIENT_ID: env('MICROSOFT_CLIENT_ID', ''),
+          CLIENT_SECRET: env('MICROSOFT_CLIENT_SECRET', ''),
+          TENANT: env('MICROSOFT_TENANT_ID', 'common'),
+          SCOPE: ['openid', 'email', 'profile'],
+          CALLBACK_URL: '/api/connect/microsoft/callback',
+        },
+      },
+    },
+  };
+
   if (!storageAccount) {
-    return {};
+    return plugins;
   }
 
   return {
+    ...plugins,
     upload: {
       config: {
         provider: 'strapi-provider-upload-azure-storage',
